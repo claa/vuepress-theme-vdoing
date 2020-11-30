cede4cdba7a135080531f4fb22e6d981daf48054
claa <736238785@qq.com>
2020-11-30 20:13:50 +0800

    20201130

--- deploy.sh
+++ deploy.sh
@@ -10,16 +10,22 @@ npm run docs:build
 cd docs/.vuepress/dist
 
 # deploy to github
+if [ -z "$GITHUB_TOKEN" ]; then
+  msg='deploy'
+  githubUrl=git@github.com:xugaoyi/vuepress-theme-vdoing.git
+else
+  msg='来自github actions的自动部署'
+  githubUrl=https://claa:${GITHUB_TOKEN}@github.com/claa/vuepress-theme-vdoing.git
+  git config --global user.name "claa"
+  git config --global user.email "736238785@qq.com"
+fi
 git init
 git add .
 git commit -m 'deploy'
-        
-
 git remote add origin https://gitee.com/claa/vuepress-theme-vdoing.git
-
 git push -u origin master
 
-git push -f git@gitee.com:claa/vuepress-theme-vdoing.git master:gh-pages
+#git push -f git@gitee.com:claa/vuepress-theme-vdoing.git master:gh-pages
 
 
 cd - # 退回开始所在目录
